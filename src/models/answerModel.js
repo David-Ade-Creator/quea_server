76f4ccb3c34ebbd59812c94df820a42cb85791cd
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-01-24 00:34:11 +0530

    changes most api to socket.io end points

--- src/models/answerModel.js
+++ src/models/answerModel.js
@@ -1,30 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+
+const LikesSchema = new mongoose.Schema(
+  {
+    wholiked: { type: mongoose.Schema.Types.ObjectId },
+  });
 
 // answer schema
 const answerScheama = new mongoose.Schema(
   {
     answer: {
       type: String,
-      required: true
+      required: true,
     },
     image: {
-        type: String,
-        trim: true
-      },
-    questionId: {
-          type: mongoose.Schema.Types.ObjectId,
-           ref: 'Question', 
-           required: true
+      type: String,
+      trim: true,
     },
-    writer : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
-    }
+    questionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+      required: true,
+    },
+    writer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [LikesSchema],
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-module.exports = mongoose.model('Answer', answerScheama);
+module.exports = mongoose.model("Answer", answerScheama);
--- src/server.js
+++ src/server.js
@@ -1,36 +1,178 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 const config = require("./config.js");
 const authRoute = require("./routes/authenticationRoutes");
 const questionRoute = require("./routes/questionRoutes");
 const answerRoute = require("./routes/answerRoutes");
 const uploadRoute = require("./routes/uploadRoutes");
 const userRoute = require("./routes/userRoutes");
+const Answer = require("./models/answerModel");
+const Question = require("./models/questionModel");
 
-const app = express()
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
 const mongodbUrl = config.MONGODB_URL;
 
-mongoose.connect(mongodbUrl,{
-    useNewUrlParser : true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-},console.log('connected')).catch(error => console.log(error.reason));
+const connect = mongoose
+  .connect(
+    mongodbUrl,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    },
+    console.log("connected")
+  )
+  .catch((error) => console.log(error.reason));
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/api/q3/",authRoute);
-app.use("/api/q3/",questionRoute);
-app.use("/api/q3/",answerRoute);
-app.use("/api/q3/",uploadRoute);
-app.use("/api/q3/",userRoute);
+app.use("/api/q3/", authRoute);
+app.use("/api/q3/", questionRoute);
+app.use("/api/q3/", answerRoute);
+app.use("/api/q3/", uploadRoute);
+app.use("/api/q3/", userRoute);
 
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static(_dirname + '/../dist'));
-// };
+io.on("connection", (socket) => {
+  socket.on("question list output", () => {
+    connect.then(async (db) => {
+      const questions = await Question.find({}).populate("whoasked");
+      if (questions) {
+        return io.emit("Output Questionlist", questions);
+      } else {
+        res.status(404).send({
+          message: "Questions Not Found.",
+        });
+      }
+    });
+  });
+  socket.on("questions", (question) => {
+    connect.then((db) => {
+      try {
+        const newQuestion = new Question({
+          question: question.question,
+          link: question.link,
+          whoasked: question.whoasked,
+        });
 
-app.listen(config.PORT, () => {
-    console.log(`server started at ${config.PORT}`);
+        newQuestion.save((err, doc) => {
+          if (err)
+            return res.json({
+              success: false,
+              err,
+            });
+
+          Question.find({})
+            .populate("whoasked")
+            .exec((err, doc) => {
+              return io.emit("Output Questionlist", doc);
+            });
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+  socket.on("saveAnswer", (answer) => {
+    connect.then((db) => {
+      const questionId = answer.questionId;
+      try {
+        let newAnswer = new Answer({
+          questionId: answer.questionId,
+          answer: answer.answer,
+          writer: answer.writer,
+        });
+
+        newAnswer.save((err, doc) => {
+          if (err)
+            return res.json({
+              success: false,
+              err,
+            });
+
+            Answer.find({
+              questionId: questionId,
+            })
+              .populate("writer")
+              .exec((err, doc) => {
+                return io.emit("savedAnswerList", doc);
+              });
+
+          Answer.find({})
+            .populate("writer")
+            .exec((err, doc) => {
+              return io.emit("Output Answerlist", doc);
+            });
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+  socket.on("answer list output", () => {
+    connect.then(async (db) => {
+      const answers = await Answer.find({}).populate(["writer", "questionId"]);
+      if (answers) {
+        return io.emit("Output Answerlist", answers);
+      } else {
+        res.status(404).send({
+          message: "Answer Not Found.",
+        });
+      }
+    });
+  });
+  socket.on("answer like", (data) => {
+    connect.then(async (db) => {
+      const { answerId, wholiked } = data;
+      const answer = await Answer.findById(answerId);
+      answer.likes.push(wholiked);
+      const likedAnswer = await answer.save();
+      if (likedAnswer) {
+        const newAnswer = await Answer.find().populate([
+          "writer",
+          "questionId",
+          "wholiked"
+        ]);
+        return io.emit("Output like", newAnswer);
+      }
+    });
+  });
+  socket.on("answer unlike", (data) => {
+    connect.then(async (db) => {
+      const { answerId, wholiked } = data;
+      const answer = await Answer.findById(answerId);
+      answer.likes.splice(answer.likes.findIndex( like => like._id === wholiked) , 1);
+      const unLikedAnswer = await answer.save();
+      if (unLikedAnswer) {
+        const newAnswer = await Answer.find().populate([
+          "writer",
+          "questionId",
+          "wholiked"
+        ]);
+        return io.emit("Output unlike", newAnswer);
+      }
+    });
+  });
+  socket.on("questionAnswers", (questionId) => {
+    connect.then(async (db) => {
+      const answers = await Answer.find({
+        questionId: questionId,
+      }).populate(["writer", "questionId"]);
+      if (answers) {
+        return io.emit("questionAnswerList", answers);
+      } else {
+        res.status(404).send({
+          message: "Answer Not Found.",
+        });
+      }
+    });
+  });
+});
+
+server.listen(config.PORT, () => {
+  console.log(`server started at ${config.PORT}`);
 });
